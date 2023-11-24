bf1d4b4660a0a47e9b127a050e08b0e37d3dc2ad
lunazzao <zhao.jiayu@northeastern.edu>
2023-11-24 01:31:51 -0800

    Personal website Project initial commit

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,35 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/about-me.png" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+            Hi! My name is Luna and I consider myself to be a future full-stack
+            software engineer with an eye for design. I have several internships
+            as full stack software engineer at Amazon Robotics and Arista
+            Networks, as well as Web/UIUX at AIA. I pride myself on being more
+            than just a developer; my background in design, which played a
+            pivotal role during my time at AIA Group, is a testament to my
+            passion for creating <b>intuitive and user-friendly </b>
+            applications. Whether working on backend development or UI design, I
+            constantly strive to build solutions that are not only robust and
+            scalable but also visually appealing and user-centric.
+          </p>
+          <p className="hero--section-description">
+            As I look forward to the next step in my career, I am eager to find
+            a role where I can continue to grow and develop as a full-stack
+            software engineer. I thrive in environments that foster teamwork,
+            collaboration, and mentorship, and I am excited about the
+            opportunity to apply my knowledge and skills to contribute to your
+            team while learning from experienced professionals in the field.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,26 @@
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hello/你好/こんにちは, I'm Luna &#127769;</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Full Stack</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+            I consider myself to be a future full-stack software engineer with an eye for design.
+            <br /> Let's connect! 
+          </p>
+        </div>
+        <button className="btn btn-primary" 
+  onClick={() => window.open('https://drive.google.com/drive/folders/1yTjjj6M2fs8Yp0HnEBHIn0CAGt-5QuPU?usp=drive_link', '_blank', 'noopener,noreferrer')}
+>Get My Resume</button>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/hero_img.png" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
